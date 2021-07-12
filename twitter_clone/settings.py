bd0e0bc54ebf8c094c01039c724efeca4654aff8
khanashes <hamzaashes@gmail.com>
2021-07-12 14:19:41 +0500

    deployed on heroku 3

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -154,3 +154,9 @@ MEDIA_URL = '/media/'
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 django_heroku.settings(locals())
+# Add these new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
